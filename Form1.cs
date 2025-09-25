040f3467e394dfa307cb299fde14de495f72f53f
Josue Abril <josue.g.abrildelcid@gmail.com>
2025-09-25 17:56:06 -0600

    Josué Abril: Documentación de mi parte

--- Form1.cs
+++ Form1.cs
@@ -14,27 +14,28 @@ namespace Administrador_de_Procesos
 
         }
 
-        private void btnDetener_Click(object sender, EventArgs e)
+        //Josue Guillermo Abril del Cid
+        private void btnDetener_Click(object sender, EventArgs e)//Boton para detener el proceso
         {
-            try
+            try//Try catch para evitar errores
             {
-                foreach (Process p in Process.GetProcesses())
+                foreach (Process p in Process.GetProcesses())//Recorrer todos los procesos
                 {
-                    if (p.ProcessName == txtProceso.Text)
+                    if (p.ProcessName == txtProceso.Text)//Si el nombre del proceso es igual al que se ingreso en el textbox
                     {
                         p.Kill(); // Matar el proceso
                     }
                 }
             }
-            catch (Exception x)
+            catch (Exception x)//Catch para mostrar el error
             {
-                MessageBox.Show("No selecicono ningun proceso" + x, "error al eliminar", MessageBoxButtons.OK);
+                MessageBox.Show("No selecicono ningun proceso" + x, "error al eliminar", MessageBoxButtons.OK);//Mensaje de error
             }
         }
 
-        private void btnSalir_Click(object sender, EventArgs e)
+        private void btnSalir_Click(object sender, EventArgs e)//Boton para salir del programa
         {
-            Close();
+            Close();//Cerrar el programa
         }
     }
 }
